1f8c76de112338836fd513b06e33783ae57e59cd
Zaki Al Faridzi <44135822+zakialfaridzi@users.noreply.github.com>
2021-04-03 00:45:36 +0700

    add input instansi di edit profile

--- application/views/profile/editProfile.php
+++ application/views/profile/editProfile.php
@@ -6,10 +6,10 @@
 		<div class="block-title">
 			<h4 class="grey"><i class="icon ion-android-checkmark-circle"></i>Edit basic information</h4>
 			<div class="line"></div>
-			<?php foreach ($info as $i) : ?>
-				<p><?= $i->name ?> was created on <?= date('d-F-y', $i->date_created); ?></p>
+			<?php foreach ($info as $i): ?>
+				<p><?=$i->name?> was created on <?=date('d-F-y', $i->date_created);?></p>
 				<div class="line"></div>
-				<?= $this->session->flashdata('message'); ?>
+				<?=$this->session->flashdata('message');?>
 		</div>
 		<div class="edit-block">
 
@@ -17,34 +17,46 @@
 
 			<div class="row">
 				<div class="form-group col-xs-12">
-					<form action="<?= base_url('profile/editBasic'); ?>" method="post">
+					<form action="<?=base_url('profile/editBasic');?>" method="post">
 						<label for="email">Full Name</label>
-						<input id="name" class="form-control input-group-lg" type="text" name="name" title="Enter Full Name" placeholder="Full Name" value="<?= $i->name; ?>" autocomplete="off" />
+						<input id="name" class="form-control input-group-lg" type="text" name="name" title="Enter Full Name" placeholder="Full Name" value="<?=$i->name;?>" autocomplete="off" />
 
 				</div>
 			</div>
 
 			<div class="row">
 				<div class="form-group col-xs-12">
-					<form action="<?= base_url('profile/editBasic'); ?>" method="post">
+					<form action="<?=base_url('profile/editBasic');?>" method="post">
 						<label for="email">Work as</label>
-						<input id="work" class="form-control input-group-lg" type="text" name="work" title="Enter Your Work" placeholder="Work" value="<?= $i->work; ?>" autocomplete="off" />
+						<input id="work" class="form-control input-group-lg" type="text" name="work" title="Enter Your Work" placeholder="Work" value="<?=$i->work;?>" autocomplete="off" />
 
 				</div>
 			</div>
 
+			<!-- </div> -->
+<?php if ($i->role_id == '3'): ?>
+	<div class="row">
+				<div class="form-group col-xs-12">
+					<form action="<?=base_url('profile/editBasic');?>" method="post">
+						<label for="email">Instansi</label>
+						<input id="work" class="form-control input-group-lg" type="text" name="instansi" title="Enter Your Institute" placeholder="Instansi" value="<?=$i->instansi;?>" autocomplete="off" />
+
+				</div>
+			</div>
+<?php endif;?>
+
 			<div class=" row">
 				<div class="form-group col-xs-12">
 					<label for="email">My email</label>
-					<input id="email" class="form-control input-group-lg" type="text" name="email" title="Enter Email" placeholder="My Email" value="<?= $i->email; ?>" autocomplete="off" />
+					<input id="email" class="form-control input-group-lg" type="text" name="email" title="Enter Email" placeholder="My Email" value="<?=$i->email;?>" autocomplete="off" />
 				</div>
-				<?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+				<?=form_error('email', '<small class="text-danger pl-3">', '</small>');?>
 			</div>
 
 			<div class="row">
 				<div class="form-group col-xs-12">
 					<label for="exampleInputEmail1">Birthday Date</label>
-					<input type="date" class="form-control" id="date" name="date" aria-describedby="emailHelp" value="<?= $i->birthdate; ?>" autocomplete="off">
+					<input type="date" class="form-control" id="date" name="date" aria-describedby="emailHelp" value="<?=$i->birthdate;?>" autocomplete="off">
 
 				</div>
 			</div>
@@ -54,35 +66,35 @@
 				<span class="custom-label"><strong>I am a: </strong></span>
 				<label class="radio-inline">
 
-					<?php if ($i->gender == 'Male') : ?>
+					<?php if ($i->gender == 'Male'): ?>
 
 						<input type="radio" name="gender" checked id="gender" value="Male">Male
 				</label>
 				<label class="radio-inline">
 					<input type="radio" name="gender" id="gender" value="Female">Female
 				</label>
-			<?php else : ?>
+			<?php else: ?>
 				<input type="radio" name="gender" id="gender" value="Male">Male
 				</label>
 				<label class="radio-inline">
 					<input type="radio" name="gender" checked id="gender" value="Female">Female
 				</label>
-			<?php endif; ?>
+			<?php endif;?>
 			</div>
 			<div class="row">
 				<div class="form-group col-xs-6">
 					<label for="city"> My city</label>
-					<input id="city" class="form-control input-group-lg" type="text" name="city" title="Enter city" placeholder="Your city" value="<?= $i->city; ?>" autocomplete="off">
+					<input id="city" class="form-control input-group-lg" type="text" name="city" title="Enter city" placeholder="Your city" value="<?=$i->city;?>" autocomplete="off">
 				</div>
 			</div>
 			<div class="row">
 				<div class="form-group col-xs-12">
 					<label for="my-info">About me</label>
-					<textarea id="bio" name="bio" class="form-control" placeholder="Some texts about me" rows="4" cols="400" autocomplete="off"><?= $i->bio; ?></textarea>
+					<textarea id="bio" name="bio" class="form-control" placeholder="Some texts about me" rows="4" cols="400" autocomplete="off"><?=$i->bio;?></textarea>
 				</div>
 			</div>
 			<button id="submit" type="submit" class="btn btn-primary" style="background-color:#0486FE; ">Save Changes</button>
-		<?php endforeach; ?>
+		<?php endforeach;?>
 		</form>
 
 
