0f8838f547b16d38cf7a8b5ff77e1e152f50e646
Fydhia Helmi <fydhiahelmi@student.telkomuniversity.ac.id>
2021-04-02 15:15:32 +0700

    chat2

--- application/views/chat/chat2.php
+++ application/views/chat/chat2.php
@@ -1,6 +1,15 @@
 <div class="chat-room">
+<?php foreach($pesan3 as $pst) : ?>
+    <h3><?php if ($pst->id_pengirim == $this->session->userdata('id')) : ?>
+        <?php else : ?>
+            <?= $pst->name; ?>'s room chat
+        <?php endif; ?>
+        </h3>
+<?php endforeach; ?>
     <div class="row">
         <div class="col-md-12">
+
+        
 
 
             <!--Chat Messages in Right-->
@@ -14,7 +23,6 @@
                                     <?php else : ?>
                                     <li class="left">
                                     <?php endif; ?>
-
                                     <div class="chat-item">
                                         <div class="chat-item-header">
                                             <h5><?= $pst->name; ?></h5>
@@ -34,12 +42,7 @@
                         <?php endforeach; ?>
 
                     </div>
-                </div>
-            </div>
-
-            <!--Chat Messages in Right End-->
-
-            <div class="send-message">
+                    <div class="send-message">
                 <div class="post-comment">
                     <?php echo smiley_js(); ?>
                     <form method="post" action="<?= base_url('Chat/kirimPesan') . "/" . $this->uri->segment('3'); ?>">
@@ -51,6 +54,10 @@
                     <?php echo $smiley_table; ?>
                 </div>
             </div>
+                </div>
+            </div>
+
+            <!--Chat Messages in Right End-->
         </div>
     </div>
 </div>
