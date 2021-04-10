a2e3ede1e3ac4345009949249670d23174bd881e
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-04-11 01:16:38 +0700

    chat

--- application/views/templates_newsfeed/footer.php
+++ application/views/templates_newsfeed/footer.php
@@ -41,25 +41,46 @@
                         </ul>
 
 
+
+
                     </div>
 
-                    <div style="width: 200px;height:60px;margin-top: 20px;">
 
 
 
-                        <div class="thumbnail">
-                            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="...">
-                            <div class="caption">
-                                <h3>Thumbnail label</h3>
-                                <p>...</p>
-                                <p><a href="#" class="btn btn-info" role="button">Button</a> <a href="#"
-                                        class="btn btn-default" role="button">Button</a></p>
+                    <div style="width: 900px;margin-top: 50px;">
+                        <div class="card" style="padding: 10px;">
+                            <div class="card-body">
+
+                                <div class="row">
+                                    <div class="col-sm-8">
+
+                                        <h4 class="card-title" style="color: black;">Nominal Dana Akun </h4>
+                                        <h3 class="card-text">Rp500.000,00</h3>
+                                        <button href="#" class="btn btn-info" style="margin-top: 10px;"><i
+                                                class="fas fa-plus"></i></button>
+
+
+                                    </div>
+
+
+                                    <div class="col-sm-4" style="margin-top: 35px;">
+                                        <div style="font-size: 80px;color: #5badff;">
+                                            <i class="fas fa-wallet fa-xs"></i>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+
+
                             </div>
                         </div>
-
-
-
                     </div>
+
+
+
                 </div>
             </div>
 
@@ -203,11 +224,6 @@ function myFunction() {
 </script>
 
 
-
-
-
-
-
 <style>
 #topBtn {
     display: none;
@@ -288,6 +304,8 @@ $(document).ready(function() {
 
 <!-- Mi
 rrored from mythemestore.com/friend-finder/newsfeed.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 21 Jan 2020 16:01:50 GMT -->
+
+
 
 
 
