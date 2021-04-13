1f0bdafbb2f62425b2cd116d362910931aa3981f
Zaki Al Faridzi <44135822+zakialfaridzi@users.noreply.github.com>
2021-04-13 10:49:39 +0700

    commit bahasa mentor

--- application/views/templates_newsfeed/headerMateri.php
+++ application/views/templates_newsfeed/headerMateri.php
@@ -6,66 +6,67 @@
 
             <!-- Newsfeed Common Side Bar Left
           ================================================= -->
-            <?php foreach ($allUser as $us) : ?>
+            <?php foreach ($allUser as $us): ?>
                 <div class="col-md-3 static">
 
                     <div class="profile-card">
-                        <img src="<?= base_url('assets_user/images/' . $us->image); ?> " alt="user" class="profile-photo" />
-                        <h5><a href="timeline.html" class="text-white"><?= $us->name; ?></a>
-                            <?php if ($us->role_id == 3) { ?>
+                        <img src="<?=base_url('assets_user/images/' . $us->image);?> " alt="user" class="profile-photo" />
+                        <h5><a href="timeline.html" class="text-white"><?=$us->name;?></a>
+                            <?php if ($us->role_id == 3) {?>
                                 <span class="badge badge-secondary">Mentor</span>
-                            <?php } ?>
+                            <?php }?>
                         </h5>
-                        <?php foreach ($jumlahfollowers as $jf) : ?>
-                            <a href="#" class="text-white"><i class="ion ion-android-person-add"></i><?= $jf->jumlahfollowers; ?> followers</a>
-                        <?php endforeach; ?>
+                        <?php foreach ($jumlahfollowers as $jf): ?>
+                            <a href="#" class="text-white"><i class="ion ion-android-person-add"></i><?=$jf->jumlahfollowers;?> followers</a>
+                        <?php endforeach;?>
                     </div>
                     <!--profile card ends-->
 
                     <ul class="nav-news-feed">
 
                         <li><i class="far fa-bell" style="color: tomato;"></i>
-                            <div><a href="<?= base_url('notification') ?>">Notification</a></div>
+                            <div><a href="<?=base_url('notification')?>">Notifikasi</a></div>
                         </li>
 
                         <li><i class="fas fa-book-reader" style="color: burlywood;"></i>
-                            <div><a href="<?= base_url('library') ?>">Material Library</a></div>
+                            <div><a href="<?=base_url('library')?>">Material Pembelajaran</a></div>
                         </li>
 
                         <li><i class="fas fa-search" style="color: peachpuff;"></i>
-                            <div><a href="<?= base_url('friend') ?>">Explore</a></div>
+                            <div><a href="<?=base_url('friend')?>">Temukan Teman</a></div>
                         </li>
 
                         <li><i class="fas fa-users" style="color: royalblue;"></i>
-                            <div><a href="<?= base_url('group') ?>">Group</a></div>
+                            <div><a href="<?=base_url('group')?>">Grup</a></div>
                         </li>
 
                         <li><i class="fas fa-comments" style="color: yellowgreen;"></i>
-                            <div><a href="<?= base_url('chat/index'); ?>">Messages</a></div>
+                            <div><a href="<?=base_url('chat/index');?>">Pesan</a></div>
                         </li>
 
                         <li><i class="fas fa-comment-dots" style="color: black;"></i>
-                            <div><a href="<?= base_url('./Chat'); ?>" target="_blank">Chat all</a></div>
+                            <div><a href="<?=base_url('./Chat');?>" target="_blank">Ngobrol Dengan Semua Pengguna</a>
+                            </div>
                         </li>
 
                         <li><i class="fa fa-video text-muted" style="color: black;"></i>
-                            <div><a href="<?= base_url('./Colab'); ?>" target="_blank">Collaboration</a></div>
+                            <div><a href="<?=base_url('./Colab');?>" target="_blank">Kolaborasi</a></div>
                         </li>
 
                     </ul>
                     <!--news-feed links ends-->
                     <div id="container1">
                         <div id="chat-block">
-                            <div class="title">Chat online</div>
+                            <div class="title">Pengguna Daring</div>
                             <ul class="online-users list-inline">
-                                <?php foreach ($otherUser as $ou) :
-                                    if ($ou->role_id != 1) {  ?>
-                                        <li>
-                                            <a href="#" title="<?= $ou->name; ?>"><img src="<?= base_url('assets_user/images/' . $ou->image); ?>" alt="user" class="img-responsive profile-photo" /><span class="<?= $ou->status; ?>" id="keyword"></span>
-                                            </a>
-                                        </li>
-                                <?php }
-                                endforeach; ?>
+                                <?php foreach ($otherUser as $ou):
+    if ($ou->role_id != 1) {?>
+				                                        <li>
+				                                            <a href="#" title="<?=$ou->name;?>"><img src="<?=base_url('assets_user/images/' . $ou->image);?>" alt="user" class="img-responsive profile-photo" /><span class="<?=$ou->status;?>" id="keyword"></span>
+				                                            </a>
+				                                        </li>
+				                                <?php }
+endforeach;?>
 
                             </ul>
                         </div>
@@ -83,11 +84,11 @@
                         <div class="row">
                             <div class="col-md-7 col-sm-7">
                                 <div class="form-group">
-                                    <?php if ($this->session->userdata('role_id') == 3 && $this->uri->segment('2') == 'materi') : ?>
-                                        <img src="<?= base_url('assets_user/images/' . $us->image); ?>" alt="" class="profile-photo-md" />
-                                        <form action="<?= base_url('library/posting/') . $this->uri->segment('3'); ?>" method="post" enctype="multipart/form-data">
+                                    <?php if ($this->session->userdata('role_id') == 3 && $this->uri->segment('2') == 'materi'): ?>
+                                        <img src="<?=base_url('assets_user/images/' . $us->image);?>" alt="" class="profile-photo-md" />
+                                        <form action="<?=base_url('library/posting/') . $this->uri->segment('3');?>" method="post" enctype="multipart/form-data">
                                             <textarea cols="30" rows="1" class="form-control" placeholder="Ayat Surat" name="ayat" id="ayat"></textarea>
-                                            <?= form_error('ayat', '<small class="text-danger pl-3">', '</small>'); ?>
+                                            <?=form_error('ayat', '<small class="text-danger pl-3">', '</small>');?>
                                 </div>
                             </div>
                             <div class="col-md-5 col-sm-5">
@@ -101,29 +102,29 @@
                                             <input type="file" id="file-input-gambar" style="display: none;" name="file">
                                         </li>
                                     </ul>
-                                    <button class="btn btn-primary pull-right" style="background-color:#6fb8df;">Publish</button>
-                                    <input type="hidden" value="<?= $this->session->userdata('id'); ?>" name="id_user" id="id_user">
-                                    <?php foreach ($idlogin as $id) : ?>
-                                        <input type="text" name="ustad" id="ustad" value="<?= $id['name']; ?>" hidden>
-                                    <?php endforeach; ?>
-                                    <?php foreach ($idpost as $idpst) : ?>
-                                        <input type="hidden" value="<?= $idpst->id_posting; ?>" name="id_posting">
-                                    <?php endforeach; ?>
+                                    <button class="btn btn-primary pull-right" style="background-color:#6fb8df;">Unggah</button>
+                                    <input type="hidden" value="<?=$this->session->userdata('id');?>" name="id_user" id="id_user">
+                                    <?php foreach ($idlogin as $id): ?>
+                                        <input type="text" name="ustad" id="ustad" value="<?=$id['name'];?>" hidden>
+                                    <?php endforeach;?>
+                                    <?php foreach ($idpost as $idpst): ?>
+                                        <input type="hidden" value="<?=$idpst->id_posting;?>" name="id_posting">
+                                    <?php endforeach;?>
                                     </form>
-                                <?php else : ?>
+                                <?php else: ?>
                                     <div class="card-header">
                                         <h2>Material Library</h2>
                                     </div>
-                                <?php endif; ?>
+                                <?php endif;?>
 
 
                                 </div>
 
-                            <?php endforeach; ?>
+                            <?php endforeach;?>
                             </div>
 
                         </div>
                     </div>
 
                     <!-- Post Create Box End
-                    <?= $this->session->flashdata('message'); ?> -->
+                    <?=$this->session->flashdata('message');?> -->
