89bac794a07772afe6222bbb47c03dd7b4b6fbd2
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-04-15 01:06:29 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://7600f3cd41b5.ap.ngrok.io/myvoqu/';
+$config['base_url'] = 'http://localhost/myvoqu/';
 // http://ae662992cfe3.ap.ngrok.io/myvoqu/
 /*
 |--------------------------------------------------------------------------
