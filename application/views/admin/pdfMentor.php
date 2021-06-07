bc04f54d8228d7825d0e4516a18ca2367bcb284f
Zaki Al Faridzi <44135822+zakialfaridzi@users.noreply.github.com>
2021-06-07 22:57:23 +0700

    pdf mentor + pdf penghafal fix

--- application/views/admin/pdfMentor.php
+++ application/views/admin/pdfMentor.php
@@ -16,9 +16,11 @@
             <tr>
                 <th>NO</th>
                 <th>Nama</th>
-                <th>Kode</th>
                 <th>Instansi</th>
                 <th>Email</th>
+                <th>Jenis Kelamin</th>
+                <th>Status Aktivasi</th>
+				<th>Status</th>
             </tr>
 
             <?php $no = 1;
@@ -26,9 +28,35 @@ foreach ($mahasiswa as $mhs): ?>
                 <tr>
                     <td><?php echo $no++; ?></td>
                     <td><?php echo $mhs->name ?></td>
-                    <td><?php echo $mhs->id ?></td>
                     <td><?php echo $mhs->instansi ?></td>
                     <td><?php echo $mhs->email ?></td>
+                    <?php if ($mhs->gender == "Male"): ?>
+						<td>
+							<?php echo "Laki Laki"; ?>
+						</td>
+						<?php else: ?>
+						<td>
+							<?php echo "Perempuan"; ?>
+						</td>
+					<?php endif;?>
+                    <?php if ($mhs->is_active == 2 || $mhs->is_active == 0): ?>
+						<td>
+							<?php echo "<span class='badge badge-danger'>Tidak Aktif</span>"; ?>
+						</td>
+						<?php else: ?>
+						<td>
+							<?php echo "<span class='badge badge-primary'>Aktif</span>"; ?>
+						</td>
+					<?php endif;?>
+					<?php if ($mhs->status == "offline-dot" || $mhs->status == ""): ?>
+						<td>
+							<?php echo "<span class='badge badge-danger'>Luring</span>"; ?>
+						</td>
+					<?php else: ?>
+						<td>
+							<?php echo "<span class='badge badge-primary'>Daring</span>"; ?>
+						</td>
+					<?php endif;?>
                 </tr>
             <?php endforeach;?>
         </table>
--- application/views/admin/pdfPenghafal.php
+++ application/views/admin/pdfPenghafal.php
@@ -18,7 +18,8 @@
                 <th>Nama</th>
                 <th>Email</th>
                 <th>Jenis Kelamin</th>
-
+                <th>Status Aktivasi</th>
+				<th>Status</th>
             </tr>
             <?php $no = 1;
 foreach ($mahasiswa as $mhs): ?>
@@ -26,7 +27,33 @@ foreach ($mahasiswa as $mhs): ?>
                     <td><?php echo $no++; ?></td>
                     <td><?php echo $mhs->name ?></td>
                     <td><?php echo $mhs->email ?></td>
-                    <td><?php echo $mhs->gender ?></td>
+                    <?php if ($mhs->gender == "Male"): ?>
+						<td>
+							<?php echo "Laki Laki"; ?>
+						</td>
+						<?php else: ?>
+						<td>
+							<?php echo "Perempuan"; ?>
+						</td>
+					<?php endif;?>
+                    <?php if ($mhs->is_active == 2 || $mhs->is_active == 0): ?>
+						<td>
+							<?php echo "<span class='badge badge-danger'>Tidak Aktif</span>"; ?>
+						</td>
+						<?php else: ?>
+						<td>
+							<?php echo "<span class='badge badge-primary'>Aktif</span>"; ?>
+						</td>
+					<?php endif;?>
+					<?php if ($mhs->status == "offline-dot" || $mhs->status == ""): ?>
+						<td>
+							<?php echo "<span class='badge badge-danger'>Luring</span>"; ?>
+						</td>
+					<?php else: ?>
+						<td>
+							<?php echo "<span class='badge badge-primary'>Daring</span>"; ?>
+						</td>
+					<?php endif;?>
                 </tr>
             <?php endforeach;?>
         </table>
